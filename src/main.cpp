6775d50e81e1d9ee4c1bf11541b630ff952712a2
Mark 5 <markvudacity@gmail.com>
2018-01-10 14:52:24 -0500

    [main] Copy over a lot of websocket code from my previous project to here.  (e.g., using constants, a sendMessage helper, manual-switch tracking, etc.)

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,12 @@
 #include "MPC.h"
 #include "json.hpp"
 
-// for convenience
 using json = nlohmann::json;
+
+// CONSTANTs
+static const int WEBSOCKECT_OK_DISCONNECT_CODE = 1000;
+static const std::string RESET_SIMULATOR_WS_MESSAGE = "42[\"reset\", {}]";
+static const std::string MANUAL_WS_MESSAGE = "42[\"manual\",{}]";
 
 // For converting back and forth between radians and degrees.
 constexpr double pi() { return M_PI; }
@@ -42,8 +46,7 @@ double polyeval(Eigen::VectorXd coeffs, double x) {
 }
 
 // Fit a polynomial.
-// Adapted from
-// https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl#L676-L716
+// Adapted from https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl#L676-L716
 Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
                         int order) {
   assert(xvals.size() == yvals.size());
@@ -65,14 +68,22 @@ Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
   return result;
 }
 
+void sendMessage(uWS::WebSocket<uWS::SERVER> ws, std::string msg) {
+  ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+}
+
 int main() {
   uWS::Hub h;
 
   // MPC is initialized here!
   MPC mpc;
 
-  h.onMessage([&mpc](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
-                     uWS::OpCode opCode) {
+  bool firstTimeConnecting = true;
+  bool justSwitchedToManual = true;
+  h.onMessage([&mpc, &justSwitchedToManual](uWS::WebSocket<uWS::SERVER> ws,
+                                            char *data,
+                                            size_t length,
+                                            uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
@@ -84,6 +95,11 @@ int main() {
         auto j = json::parse(s);
         string event = j[0].get<string>();
         if (event == "telemetry") {
+          if (!justSwitchedToManual) {
+            std::cout << "Taking back control from manual!!" << std::endl;
+            justSwitchedToManual = true;
+          }
+
           // j[1] is the data JSON object
           vector<double> ptsx = j[1]["ptsx"];
           vector<double> ptsy = j[1]["ptsy"];
@@ -137,15 +153,17 @@ int main() {
           // Feel free to play around with this value but should be to drive
           // around the track with 100ms latency.
           //
-          // NOTE: REMEMBER TO SET THIS TO 100 MILLISECONDS BEFORE
-          // SUBMITTING.
+          // NOTE: REMEMBER TO SET THIS TO 100 MILLISECONDS BEFORE SUBMITTING.
           this_thread::sleep_for(chrono::milliseconds(100));
-          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+          sendMessage(ws, msg);
         }
       } else {
-        // Manual driving
-        std::string msg = "42[\"manual\",{}]";
-        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+        if (justSwitchedToManual) {
+            justSwitchedToManual = false;
+            std::cout << "Switched to manual mode!!" << std::endl;
+        }
+
+        sendMessage(ws, MANUAL_WS_MESSAGE);
       }
     }
   });
@@ -153,7 +171,9 @@ int main() {
   // We don't need this since we're not using HTTP but if it's removed the
   // program
   // doesn't compile :-(
-  h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data,
+  h.onHttpRequest([](uWS::HttpResponse *res,
+                     uWS::HttpRequest req,
+                     char *data,
                      size_t, size_t) {
     const std::string s = "<h1>Hello world!</h1>";
     if (req.getUrl().valueLength == 1) {
@@ -164,14 +184,70 @@ int main() {
     }
   });
 
-  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
-    std::cout << "Connected!!!" << std::endl;
+  h.onConnection([&h, &firstTimeConnecting](uWS::WebSocket<uWS::SERVER> ws,
+                                            uWS::HttpRequest req) {
+      if (firstTimeConnecting) {
+          std::cout << "Connected for first time!!!  Restarting simulator!" << std::endl;
+          firstTimeConnecting = false;
+          sendMessage(ws, RESET_SIMULATOR_WS_MESSAGE);
+      } else {
+          std::cout << "Reconnected to simulator, success!" << std::endl;
+      }
   });
 
-  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
-                         char *message, size_t length) {
-    ws.close();
-    std::cout << "Disconnected" << std::endl;
+  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws,
+                         int code,
+                         char *message,
+                         size_t length) {
+      if (code == WEBSOCKECT_OK_DISCONNECT_CODE) {
+        std::cout << "Disconnected normally." << std::endl;
+      } else {
+        std::cout << "Unexpected Disconnect with code: " << code << "!" << std::endl;
+      }
+
+      std::cout << "WARN: Not closing WS because we get bad access exception! (Which one cannot catch in C++!?)"  << std::endl;
+      // StackOverflow https://stackoverflow.com/q/19304157 suggested that error code 1006 means to check onError
+      // But, but, but, adding onError here doesn't get called at all, everything seems alright
+      // (other than this ws.close() exc_bad_access)
+      // ws.close(code, message, length);
+      // Besides ^^, if we're getting disconnection method, then the connection is already closed??
+  });
+
+  h.onError([](void *user) {
+      // Code copied from: https://github.com/uNetworking/uWebSockets/blob/master/tests/main.cpp
+      switch ((long) user) {
+        case 1:
+          std::cout << "Client emitted error on invalid URI" << std::endl;
+              break;
+        case 2:
+          std::cout << "Client emitted error on resolve failure" << std::endl;
+              break;
+        case 3:
+          std::cout << "Client emitted error on connection timeout (non-SSL)" << std::endl;
+              break;
+        case 5:
+          std::cout << "Client emitted error on connection timeout (SSL)" << std::endl;
+              break;
+        case 6:
+          std::cout << "Client emitted error on HTTP response without upgrade (non-SSL)" << std::endl;
+              break;
+        case 7:
+          std::cout << "Client emitted error on HTTP response without upgrade (SSL)" << std::endl;
+              break;
+        case 10:
+          std::cout << "Client emitted error on poll error" << std::endl;
+              break;
+        case 11:
+          static int protocolErrorCount = 0;
+              protocolErrorCount++;
+              std::cout << "Client emitted error on invalid protocol" << std::endl;
+              if (protocolErrorCount > 1) {
+                std::cout << "FAILURE:  " << protocolErrorCount << " errors emitted for one connection!" << std::endl;
+              }
+              break;
+        default:
+          std::cout << "FAILURE: " << user << " should not emit error!" << std::endl;
+      }
   });
 
   int port = 4567;
